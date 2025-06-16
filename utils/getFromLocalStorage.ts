a966afe232698328c1edfc94d83ebcd73f7e0c5d
nichgalzin <dropout@Nichs-MacBook-Air.local>
2025-06-16 14:39:40 +0100

    feat: util for fetching items from local storage with error and type checking
    Task 2.4

---
+++ utils/getFromLocalStorage.ts
@@ -0,0 +1,9 @@
+export const getFromLocalStorage = <T>(key: string): T | null => {
+  try {
+    const item = localStorage.getItem(key);
+    return item ? (JSON.parse(item) as T) : null;
+  } catch (error) {
+    console.error(`Error retrieving data from localStorage: ${error}`);
+    return null;
+  }
+};
